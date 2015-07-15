c6fc04867ab96413d488c730b850940fdbcaee07
giusep1995 <giusep1995@gmail.com>
2015-07-15 10:42:26 +0200

    Migliorata animazione uscita pokemon

---
+++ Assets/Scripts/Intro/Intro.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using AncientTimes.Assets.Scripts.GameSystem;
+using System.Collections;
+
+namespace AncientTimes.Assets.Scripts.GameSystem
+{
+    public class Intro : MonoBehaviour
+    {
+        #region Properties
+
+        Animator animator;
+        GameObject Kerneth;
+        #endregion Properties
+
+        #region Methods
+        void Start()
+        {
+            Kerneth = GameObject.Find("KernethSprite");
+            animator = Kerneth.GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            Console.Write("Ciao");
+            if (Input.GetKeyDown("t"))
+            {
+                animator.SetBool("isOut", true);
+            }
+        }
+
+        #endregion Methods
+
+    }
+}
