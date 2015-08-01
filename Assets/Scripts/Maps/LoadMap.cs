d156e5b0520c19946eef889a1c543d74fd30ca61
giusep1995 <giusep1995@gmail.com>
2015-08-02 01:19:09 +0200

    Piccole migliorie

--- Assets/Scripts/Maps/LoadMap.cs
+++ Assets/Scripts/Maps/LoadMap.cs
@@ -7,18 +7,14 @@ namespace AncientTimes.Assets.Scripts.WildPokemon
     public class LoadMap : MonoBehaviour
     {
         private string currentNameMap;
-        private GameObject Places;
 
         // Use this for initialization
         void Start()
         {
-            Places = GameObject.Find("Places");
             Destroy(GameObject.FindGameObjectWithTag("Places"));
             currentNameMap = GameVariables.GetVariable("CurrentMap");
             Debug.Log(currentNameMap);
             GameObject instance = Instantiate(Resources.Load(  "Maps/"+ currentNameMap, typeof(GameObject))) as GameObject;
-            instance.transform.parent = Places.transform;
-            instance.transform.localPosition = new Vector3(0, 0, 0);
         }
     }
 }
