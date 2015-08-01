0c0aae0c5d7111c77266a5163a1163a7831bddc5
demitore <demi.tore@gmail.com>
2015-08-01 15:44:50 +0200

    [DMS] Test Load Map

---
+++ Assets/Scripts/Maps/LoadMap.cs
@@ -0,0 +1,28 @@
+﻿using AncientTimes.Assets.Scripts.Utilities;
+using UnityEngine;
+using System.Collections;
+
+namespace AncientTimes.Assets.Scripts.WildPokemon
+{
+    public class LoadMap : MonoBehaviour
+    {
+        private string currentNameMap;
+
+        // Use this for initialization
+        void Start()
+        {
+            currentNameMap = GameVariables.Variables["CurrentMap"];
+            Debug.Log(currentNameMap);
+            //GameObject instance = Instantiate(Resources.Load( @"Assets/Prefab/Maps/"+ currentNameMap, typeof(GameObject))) as GameObject;
+            GameObject prefab = Resources.Load<GameObject>(@"Assets/Prefab/Maps/" + currentNameMap + ".prefab");
+            GameObject newMap = (GameObject)Instantiate(prefab);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
