b07329ad004f6b697915e0e0fce3f2dd8357ef57
giusep1995 <giusep1995@gmail.com>
2015-08-01 21:11:15 +0200

    Update LoadMap.cs

--- Assets/Scripts/Maps/LoadMap.cs
+++ Assets/Scripts/Maps/LoadMap.cs
@@ -1,4 +1,4 @@
-﻿using AncientTimes.Assets.Scripts.Utilities;
+using AncientTimes.Assets.Scripts.Utilities;
 using UnityEngine;
 using System.Collections;
 
@@ -7,13 +7,19 @@ namespace AncientTimes.Assets.Scripts.WildPokemon
     public class LoadMap : MonoBehaviour
     {
         private string currentNameMap;
+        private GameObject Places;
 
         // Use this for initialization
         void Start()
         {
+            Places = GameObject.Find("Places");
+            Destroy(GameObject.FindGameObjectWithTag("Places"));
             currentNameMap = GameVariables.Variables["CurrentMap"];
             Debug.Log(currentNameMap);
-            GameObject instance = Instantiate(Resources.Load(currentNameMap, typeof(GameObject))) as GameObject;
+            GameObject instance = Instantiate(Resources.Load(  "Maps/"+ currentNameMap, typeof(GameObject))) as GameObject;
+            instance.transform.parent = Places.transform;
+            instance.transform.localPosition = new Vector3(0, 0, 0);
+
         }
 
         // Update is called once per frame
@@ -21,6 +27,7 @@ namespace AncientTimes.Assets.Scripts.WildPokemon
         {
 
         }
+
     }
 
 }
