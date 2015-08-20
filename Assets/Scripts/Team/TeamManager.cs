fb667cd99474cc694b46301b60a1dc6aeef31a29
Yariet <miki.maneggia@tiscali.it>
2015-08-20 10:23:53 +0200

    Team - Added Team Manager class

---
+++ Assets/Scripts/Team/TeamManager.cs
@@ -0,0 +1,33 @@
+﻿using AncientTimes.Assets.Scripts.Utilities;
+using Assets.Scripts.Utilities;
+using UnityEngine;
+
+namespace Assets.Scripts.Team
+{
+    public class TeamManager : MonoBehaviour {
+
+        public enum TeamType
+        {
+            Battle,
+            Menu
+        }
+
+        private TeamType teamType;
+
+        //lista squadra
+        
+        void Start ()
+        {
+            if (ScenesCommunicator.IsBattleTeam)
+                teamType = TeamType.Battle;
+            else teamType = TeamType.Menu;
+
+            
+        }
+	
+        
+        void Update () {
+	
+        }
+    }
+}
