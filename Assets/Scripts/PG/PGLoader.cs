82b2adf8763afe69948830dfdbc9443ad4459ac5
demitore <demi.tore@gmail.com>
2015-09-05 20:43:29 +0200

    [DMS] Aggiunto Animator Clarity (Female) e PG Loader per la scelta Machio/Femmina

---
+++ Assets/Scripts/PG/PGLoader.cs
@@ -0,0 +1,22 @@
+﻿using AncientTimes.Assets.Scripts.GameSystem;
+using AncientTimes.Assets.Scripts.Utilities;
+using UnityEngine;
+using System.Collections;
+
+namespace AncientTimes.Assets.Scripts.Intro
+{
+    public class PGLoader : MonoBehaviour
+    {
+        #region Methods
+
+        void Start() { Load((GameVariables.Get("IsMan", true))); }
+
+        public static void Load(string isMan)
+        {
+            if (isMan.Equals("True")) Instantiate(Resources.Load("PG/" + "Leon", typeof(GameObject)));
+            else Instantiate(Resources.Load("PG/" + "Clarity", typeof(GameObject)));
+        }
+
+        #endregion Methods
+    }
+}
