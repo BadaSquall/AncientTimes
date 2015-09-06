5bcd3a15a0c99be57382d3b7e20257ee85bb107b
demitore <demi.tore@gmail.com>
2015-09-06 02:21:02 +0200

    [DMS] Load Player con coordinate & Load Non-Player (controparte del PG)

---
+++ Assets/Scripts/PG/NPLoader.cs
@@ -0,0 +1,38 @@
+﻿using AncientTimes.Assets.Scripts.Utilities;
+using UnityEngine;
+using System.Collections;
+
+namespace AncientTimes.Assets.Scripts.PG
+{
+    public class NPLoader : MonoBehaviour
+    {
+        #region Properties
+
+        private GameObject NPIstantiated;
+
+        #endregion Properties
+
+        #region Methods
+
+        void Start()
+        {
+            Load((GameVariables.Get("IsMan", true)));
+        }
+
+        void Load(string isMan)
+        {
+            if (isMan.Equals("True")) Instantiate(Resources.Load("PG/" + "NPClarity", typeof(GameObject)));
+            else Instantiate(Resources.Load("PG/" + "NPLeon", typeof(GameObject)));
+
+            NPIstantiated = GameObject.FindGameObjectWithTag("NonPlayer");
+            SetPosition(NPIstantiated, this.gameObject.transform.position);
+        }
+
+        void SetPosition(GameObject player, Vector3 coordinates)
+        {
+            player.transform.position = coordinates;
+        }
+
+        #endregion Methods
+    }
+}
--- Assets/Scripts/PG/PGLoader.cs
+++ Assets/Scripts/PG/PGLoader.cs
@@ -3,18 +3,41 @@ using AncientTimes.Assets.Scripts.Utilities;
 using UnityEngine;
 using System.Collections;
 
-namespace AncientTimes.Assets.Scripts.Intro
+namespace AncientTimes.Assets.Scripts.PG
 {
     public class PGLoader : MonoBehaviour
     {
+
+        #region Properties
+
+        private GameObject PlayerIstantiated;
+        private string PlayerPositionX;
+        private string PlayerPositionY;
+
+        #endregion Properties
+
         #region Methods
 
-        void Start() { Load((GameVariables.Get("IsMan", true))); }
+        void Start()
+        {
+            Load((GameVariables.Get("IsMan", true)));
+        }
 
-        public static void Load(string isMan)
+        void Load(string isMan)
         {
             if (isMan.Equals("True")) Instantiate(Resources.Load("PG/" + "Leon", typeof(GameObject)));
             else Instantiate(Resources.Load("PG/" + "Clarity", typeof(GameObject)));
+
+            PlayerIstantiated = GameObject.FindGameObjectWithTag("Player");
+            PlayerPositionX = GameVariables.Get("PlayerPositionX",this.gameObject.transform.position.x);
+            PlayerPositionY = GameVariables.Get("PlayerPositionY", this.gameObject.transform.position.y);
+
+            SetPosition(PlayerIstantiated, float.Parse(PlayerPositionX), float.Parse(PlayerPositionY));
+        }
+
+        void SetPosition(GameObject player, float x, float y)
+        {
+            player.transform.position = new Vector3(x, y);
         }
 
         #endregion Methods
