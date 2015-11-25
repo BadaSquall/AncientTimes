eaf14897771ae7430439e41ee51a8104d81289f1
giusep1995 <giusep1995@gmail.com>
2015-11-26 00:44:56 +0100

    avanzamento nella scena iniziale

--- Assets/Scripts/Warps/WarpToMap.cs
+++ Assets/Scripts/Warps/WarpToMap.cs
@@ -22,7 +22,7 @@ namespace AncientTimes.Assets.Scripts.WildPokemon
 
         private void Start() {  }
 
-        private void FixedUpdate() { if (IsIn) MapLoader.Load(mapToWarp); }
+        private void FixedUpdate() { if (IsIn) ChangeMap(); }
 
         private void OnTriggerEnter2D(Collider2D coll) { IsIn = true; }
 
@@ -30,6 +30,11 @@ namespace AncientTimes.Assets.Scripts.WildPokemon
 
         private void OnTriggerExit2D(Collider2D coll) { IsIn = false; }
 
+        private void ChangeMap()
+        {
+            MapLoader.Load(mapToWarp);
+            GameObject.Find("Player").transform.position = new Vector2(GameObject.Find(EndWarp).transform.position.x, GameObject.Find(EndWarp).transform.position.y);
+        }
         #endregion Methods
     }
 }
