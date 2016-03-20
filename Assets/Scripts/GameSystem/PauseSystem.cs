dafa6c3f0792724d7592b0a2a674e6127c0ee9c5
giusep1995 <giusep1995@gmail.com>
2016-03-20 01:18:47 +0100

    Pause Menu Modified

--- Assets/Scripts/GameSystem/PauseSystem.cs
+++ Assets/Scripts/GameSystem/PauseSystem.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using AncientTimes.Assets.Scripts.Utilities;
 
 namespace AncientTimes.Assets.Scripts.GameSystem
@@ -8,13 +9,26 @@ namespace AncientTimes.Assets.Scripts.GameSystem
     {
 	    #region Properties
 
-        public GUIStyle ButtonStyle;
+        private GameObject PauseMenu;
+        private GameObject Save;
+        private GameObject Squad;
+        private GameObject Bag;
+        private GameObject Pokedex;
 
 	    #endregion Properties
 
 	    #region Methods
 
-	    // Update is called once per frame
+        void Start()
+        {
+            PauseMenu = GameObject.Find("PauseMenu");
+            PauseMenu.SetActive(false);
+            Save.SetActive(false);
+            Squad.SetActive(false);
+            Bag.SetActive(false);
+            Pokedex.SetActive(false);
+        }
+
 	    void Update () { if (Input.GetKeyDown (KeyCode.Escape)) Pause(); }
 
 	    void Pause()
@@ -23,30 +37,43 @@ namespace AncientTimes.Assets.Scripts.GameSystem
             {
                 GameVariables.Update("IsPaused", true);
                 Time.timeScale = 0f;
+                PauseMenu.SetActive(true);
             }
             else if (bool.Parse(GameVariables.Get("IsPaused", false)))
             {
                 GameVariables.Update("IsPaused", false);
                 Time.timeScale = 1f;
+                PauseMenu.SetActive(false);
             }
         }
 
-	    void OnGUI()
-	    {
-            if (bool.Parse(GameVariables.Get("IsPaused", false)))
+        void LoadButtons()
+        {
+            GameObject.Find("Bag").GetComponent<Button>().onClick.AddListener(DoThing("Bag"));
+            GameObject.Find("Exit").GetComponent<Button>().onClick.AddListener(DoThing("Exit"));
+            GameObject.Find("Squad").GetComponent<Button>().onClick.AddListener(DoThing("Squad"));
+            GameObject.Find("Save").GetComponent<Button>().onClick.AddListener(DoThing("Save"));
+            GameObject.Find("Pokedex").GetComponent<Button>().onClick.AddListener(DoThing("Pokedex"));
+        }
+
+        UnityEngine.Events.UnityAction DoThing(string thing)
+        {
+            switch (thing)
             {
-                GUILayout.BeginArea(new Rect(Screen.width / 2 -100 , Screen.height / 5 -50 , Screen.width, Screen.height));
-                GUILayout.Button("Pokedex", ButtonStyle);
-                GUILayout.Button("Pokemon", ButtonStyle);
-                GUILayout.Button("Zaino", ButtonStyle);
-                GUILayout.Button("Save", ButtonStyle);
-
-                if (GUILayout.Button("Esci dal gioco", ButtonStyle)) Application.Quit();
-
-                GUILayout.EndArea();
+                case "Bag": Bag.SetActive(true);
+                    break;
+                case "Exit":
+                    break;
+                case "Squad": Squad.SetActive(true);
+                    break;
+                case "Save": Save.SetActive(true);
+                    break;
+                case "Pokedex": Pokedex.SetActive(true);
+                    break; 
             }
-            else if (!bool.Parse(GameVariables.Get("IsPaused", false))) GUI.enabled = false;
-	    }   
+            PauseMenu.SetActive(false);
+            return null;
+        }
 
 	    #endregion Methods
     }
